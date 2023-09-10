0b2d09501b2f7dd1f3be217df0163bfc988ebac7
tmctrevor <mohamed.james1949@gmail.com>
2023-09-10 15:11:10 +0100

    datetime

--- Python0-Starting/ex01/format_fit_time.py
+++ Python0-Starting/ex01/format_fit_time.py
@@ -1,5 +1,25 @@
 import datetime
+from datetime import timedelta
+delta = timedelta(
+    days=50,
+    seconds=27,
+    microseconds=10,
+    milliseconds=29000,
+    minutes=5,
+    hours=8,
+    weeks=2
+)
+date3 = datetime.date(2020,9,7)
+dayAfter = date3 + delta
 
 
-date = datetime.datetime.now()
-print(date.month, date.day, date.year)
+
+# date = datetime.datetime.now()
+# date2 = datetime.datetime(2020,9,7)
+
+
+
+print(dayAfter)
+# print(date3)
+# print(datetime.date.year)
+# print(date.month, date.day, date.year)
